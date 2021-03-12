10a63524d30095b9ce4e2bb4b9536348b78b4101
AritraStark <58631915+AritraStark@users.noreply.github.com>
2021-03-12 19:07:33 +0530

    following route added

--- backend/controller/followerController.js
+++ backend/controller/followerController.js
@@ -55,6 +55,24 @@ export const getFollowersCount = asyncHandler(async (req, res) => {
     }
 })
 
+//@route GET /api/followers/following/:id
+//@desc Get count of followings
+//@access Private
+export const getFollowingCount = asyncHandler(async (req, res) => {
+    const id = req.params.id
+
+    const { rowCount } = await query('SELECT * FROM usersdb WHERE id = $1 ', [id])
+    if (rowCount == '1') {
+        const { rows } = await query('SELECT COUNT(id) FROM followersdb WHERE user_id_secondary = $1', [id])
+
+        res.status(200)
+            .json(rows[0])
+    } else {
+        res.status(400)
+        throw new Error('Invalid data')
+    }
+})
+
 //@route GET /api/followers/:id
 //@desc Get count of followers
 //@access Private
--- backend/routes/followerRoutes.js
+++ backend/routes/followerRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteFollower, getFollowers, getFollowersCount, insertFollower } from '../controller/followerController.js'
+import { deleteFollower, getFollowers, getFollowersCount, getFollowingCount, insertFollower } from '../controller/followerController.js'
 import authM from '../middleware/authMiddleware.js'
 
 const router = express.Router()
@@ -7,5 +7,6 @@ const router = express.Router()
 router.post('/', authM, insertFollower)
 router.route('/:id').get(authM, getFollowers).delete(authM, deleteFollower)
 router.get('/count/:id', authM, getFollowersCount)
+router.get('/following/:id', authM, getFollowingCount)
 
 export default router
